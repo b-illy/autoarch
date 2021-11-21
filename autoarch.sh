90c95718ca8d79cc0868f8df503f00174b2380f6
billy <19840447+b-illy@users.noreply.github.com>
2021-11-21 17:52:09 +0000

    add coloured section titles

--- autoarch.sh
+++ autoarch.sh
@@ -1,14 +1,21 @@
+autoload colors; colors  # allow coloured echo commands
+
+section() {  # function to easily print section titles
+    clear
+    echo $fg_bold[cyan] $1
+    echo ${reset_color} "-----------------------------------------------------"
+    echo
+}
+
 if [ "$EUID" != "0" ]; then
     echo "ERROR: Must be ran as root user in an archlinux live environment"
     exit
 fi
 
-clear
+section "AutoArch - Arch Linux installation script"
+read "?Press ENTER to start"
 
-echo "AutoArch - Arch Linux installation script"
-echo "-----------------------------------------"
-echo -ne "\nPress ENTER to start"
-read
+section "Initial checks and setup"
 
 # check if booted in efi or bios mode
 if ls /sys/firmware/efi/efivars > /dev/null 2>&1; then
@@ -25,7 +32,7 @@ echo " mode, if this was unexpected, check motherboard settings to make sure you
 # check if there is already an internet connection
 echo -e "\nChecking internet connection..."
 if curl https://archlinux.org > /dev/null 2>&1; then
-    echo -e "\nInternet connection working, continuing..."
+    echo "Internet connection working, continuing..."
 else
     echo "Internet connection doesn't seem to be working, entering troubleshooting..."
     while [ true ]; do
@@ -65,24 +72,22 @@ echo -ne "\nPlease enter a keyboard layout: "
 read keymap
 loadkeys "$keymap" > /dev/null 2>&1
 
-clear
 
+# partitioning
+section "Partitioning"
 
 # determine a decent swap amount
 physmem=0
 physmem=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 swap=0
-
-
 if [ $physmem -lt 6000000 ]; then
     swap=$((($physmem+500000)/1000000))
 elif [ $physmem -lt 12000000 ]; then
     swap=4
 fi
 
-
 # show recommended partitions (no separate home partition)
-echo -e "\nSetting up partitions, recommended layout for you:"
+echo "Setting up partitions, recommended layout for you:"
 if [ $efi = "true" ]; then
     echo "(gpt)"
     echo "efi partition (fat32) (512 MiB) (/mnt/boot) "
@@ -93,7 +98,6 @@ if [ $swap != 0 ]; then
     echo "swap partition (linux swap) ($swap GiB)"
 fi
 echo "root fs (ext4) (all remaining free space on the disk) (/mnt)"
-
 
 # chance to modify swap amount
 while [ true ]; do
@@ -109,18 +113,18 @@ while [ true ]; do
     fi
 done
 
-
 # select device to partition
-echo -e "\n\nList of connected storage devices:"
+section "Partitioning"
+echo "List of connected storage devices:"
 lsblk -S
-read dev"?Choose a disk from this list to partition (e.g. 'sda' or 'nvme0n1'): "
+read dev"?Choose a disk from this list to partition (e.g. 'sda' or 'nvme0n1'): /dev/"
 devp=$dev # ensure partitions on nvme drives (nvme0n1 -> nvme0n1p1 vs sda -> sda1) are referred to correctly
 if [[ $dev == nvme* ]]; then
     devp="${dev}p"
 fi
 
-
 # select partition tool / method
+section "Partitioning"
 while [ true ]; do
     read manualpart"?Would you like to partition manually (enter 1) or automatically (enter 2)? "
     if [ $manualpart = "1" ]; then
@@ -145,15 +149,11 @@ while [ true ]; do
         echo "Now that you have set up partitions, you will have to format and mount them"
         echo "Press alt + left/right arrow to switch between terminals"
         read "?Press ENTER when you have finished to continue"
-        
         break
-        
     elif [ $manualpart = "2" ]; then
         # automatic partitioning
-
         if [ $efi = "false" ]; then
             # mbr / bios
-
             parted /dev/$dev mklabel msdos
 
             if [ $swap -ne 0 ]; then  # with swap
@@ -170,9 +170,7 @@ while [ true ]; do
             fi
         else
             # gpt / efi
-
             parted /dev/$dev mklabel gpt
-
             parted /dev/$dev mkpart boot fat32 2MiB 514MiB
             parted /dev/$dev set 1 boot
             parted /dev/$dev set 1 esp
@@ -190,27 +188,23 @@ while [ true ]; do
                 mkfs.ext4 /dev/${devp}2
                 mount /dev/${devp}2 /mnt
             fi
-
             mkdir -p /mnt/boot
             mount /dev/${devp}1 /mnt/boot
         fi
-        
         break
-        
     else
         echo "invalid input"
     fi
 done
 
-clear
-
 
 # choose linux kernel version to use
+section "Main system setup"
 while [ true ]; do
-    echo -e "Ready to install the core packages. Please select your preferred kernel:"
+    echo "Please select your preferred kernel:"
     echo "1) Linux - most updated kernel version"
-    echo "2) Linux LTS - stable release, updated less often, may be good for old hardware support"
-    echo "3) Linux Hardened - security-focused branch"
+    echo "2) Linux LTS - stable release, updated less often"
+    echo "3) Linux Hardened - very security-focused branch, fewer features"
     echo "4) Linux Zen - optimised for performance"
     read "?Your choice of kernel (enter a number): "
     if [ $REPLY = "1" ]; then
@@ -230,24 +224,24 @@ while [ true ]; do
     fi
 done
 
-clear
-
 
 # install core system + useful packages
+section "Main system setup"
 echo "Ready to install core packages and set up the system (will likely take several minutes)"
 read "?Press ENTER when you are ready"
+# install packages
 pacstrap /mnt base $kernel linux-firmware networkmanager xorg neofetch git base-devel grub efibootmgr os-prober btrfs-progs dosfstools e2fsprogs ntfs-3g xfsprogs nano vim man-db man-pages texinfo --noconfirm
 pacman -Sy
 pacman -S git --noconfirm  # also install git to live environment to install yay later
-
+# other useful setup stuff
 echo "KEYMAP=${keymap}" > /mnt/etc/vconsole.conf  # save keymap across reboots on new system
 genfstab -U /mnt >> /mnt/etc/fstab  # generate fs table for partitions to actually get mounted
 arch-chroot /mnt systemctl enable NetworkManager
 
-clear
-
 
 # setup timezone
+clear
+
 (
     echo "This is a list of all available timezones and the filepaths to them"
     echo -e "Take note of the path (after /usr/share/zoneinfo/) for your timezone (e.g. Europe/London)\n"
@@ -259,10 +253,9 @@ read timezone
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
 arch-chroot /mnt hwclock --systohc
 
-clear
-
 
 # setup locale - locale.gen
+section "Locale setup"
 echo "Setting up locale..."
 echo "You will have to uncomment (removing the '#' at the start of the line) any desired locales."
 echo "It is recommend to only use the UTF8 variants and also to include en_US.UTF8"
@@ -270,47 +263,39 @@ echo "Remember your main locale's name (everything before the first space) as yo
 read "?Press ENTER when you are ready"
 nano /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-    
-clear
 
 
-# setup locale - choose main locale
+# setup locale - choose main locale    
+section "Locale setup"
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 echo "You will now have to replace en_US.UTF8 with your desired primary locale"
 read "?Press ENTER when you are ready"
 nano /mnt/etc/locale.conf
 
-clear
-
 
 # initramfs
-echo "Setting up initramfs..."
+section "initramfs"
 arch-chroot /mnt mkinitcpio -P
-
-clear
 
 
 # grub install
-echo -e "Installing GRUB (bootloader)...\n"
+section "GRUB install and setup"
 if [ $efi = "true" ]; then
     arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 else
     arch-chroot /mnt grub-install --target=i386-pc /dev/$dev
 fi
 
-clear
-
 
 # grub config
+section "GRUB install and setup"
 echo -e "You will now have a chance to edit your GRUB config\nIf you are fine with the default, just exit nano with Ctrl-X"
 read "?Press ENTER when you are ready"
 nano /mnt/etc/default/grub
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-clear
-
-
 # setup users + passwords
+section "Local accounts setup"
 echo -ne "Creating a non-root sudoer user.\nEnter desired username (cannot contain spaces): "
 read username
 arch-chroot /mnt useradd -m -G wheel -s /bin/bash $username
@@ -322,32 +307,27 @@ echo -e "\nSetting up password for root user:"
 echo -e "root\nroot\n" | arch-chroot /mnt passwd > /dev/null 2>&1 # setup default password so login possible if passwd fails
 arch-chroot /mnt passwd
 
-clear
-
 
 # choose hostname
-echo -n "Choose a hostname for your computer, such as '${username}-pc': "
+section "Setup machine hostname"
+echo -ne "\nChoose a hostname for your computer, such as '${username}-pc': "
 read hostname
 echo $hostname > /mnt/etc/hostname
 echo "127.0.0.1 localhost" >> /mnt/etc/hosts
 echo "::1 localhost" >> /mnt/etc/hosts
 
-clear
-
 
 # setup sudo
-echo "Setting up sudo..."
+section "Sudo setup"
 mv /mnt/etc/sudoers /mnt/etc/sudoers.bak
 echo "## setup by a script. see /etc/sudoers.bak for the default file." > /mnt/etc/sudoers
 echo "root ALL=(ALL) ALL" >> /mnt/etc/sudoers
 echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 echo "@includedir /etc/sudoers.d" >> /mnt/etc/sudoers
 
-clear
-
 
 # install yay
-echo "Installing yay..."
+section "Yay (AUR helper) installation"
 git clone https://aur.archlinux.org/yay-bin.git
 mv yay-bin /mnt/home/${username}/yay-bin
 arch-chroot /mnt chmod a+w /home/${username} /home/${username}/yay-bin  # makepkg needs specific perms
@@ -355,12 +335,10 @@ arch-chroot /mnt sudo -u $username bash -c "cd /home/${username}/yay-bin && make
 rm -rf /mnt/home/${username}/yay-bin
 arch-chroot /mnt chmod 774 /home/${username}  # make sure perms are alright
 
-clear
-
 
 # desktop environment
+section "Desktop environment installation"
 while [ true ]; do
-    echo "Desktop environment:"
     echo -e "1) KDE Plasma (recommended for beginners)\n2) Xfce4\n3) LXQt\n4) GNOME\n5) Cinnamon\n6) None / manual install"
     echo "All installs will also include at least a display manager, filemanager and terminal emulator"
     read "?Select one of the above options to install (enter a number): "
@@ -391,12 +369,10 @@ while [ true ]; do
     fi
 done
 
-clear
-
 
 # browser
+section "Web browser installation"
 while [ true ]; do
-    echo -e "\nWeb browser:"
     echo -e "1) Firefox\n2) Chromium\n3) qutebrowser\n4) Vivaldi\n5) None / manual install"
     read "?Choose a browser to install (enter a number): "
     if [ $REPLY = "1" ]; then
@@ -418,8 +394,9 @@ while [ true ]; do
     fi
 done
 
-clear
 
+# vm guest stuff
+section "VM guest tools setup"
 
 # vmware
 if dmesg | grep -i "manufacturer: vmware" > /dev/null 2>&1; then
@@ -454,10 +431,9 @@ if dmesg | grep -i "manufacturer: virtualbox" > /dev/null 2>&1; then
     done
 fi
 
-clear
-
 
 # fonts
+section "Optional fonts"
 while [ true ]; do
     read "?Would you like to install a large collection of fonts (enter 1) or just use the preinstalled ones for now (enter 2)? "
     if [ $REPLY = "1" ]; then
@@ -470,10 +446,9 @@ while [ true ]; do
     fi
 done
 
-clear
-
 
 # microcode
+section "CPU microcode patches"
 while [ true ]; do
     echo -e "CPU microcode patches:\n1) Intel\n2) AMD\n3) None"
     read "?Choose which CPU microcode patches to install (enter a number): "
@@ -496,14 +471,12 @@ while [ true ]; do
     fi
 done
 
-clear
-
 
 # finishing up
+section "END"
 echo -e "All done!\nYou should be good to reboot now\n"
-
 while [ true ]; do
-    read "?Reboot now (enter 1) or later manually (using the 'reboot' command) (enter 2)? "
+    read "?Reboot now (1=yes, 2=no)? "
     if [ $REPLY = "1" ]; then
         reboot
         break
